051699612d381a908f8da9eff9f0f79c871702d8
mahfuz-7148 <mrahman7148@gmail.com>
2025-06-29 12:18:42 +0600

    working

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const { MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
 const express = require('express')
 const cors = require('cors')
-const jwt = require('jsonwebtoken')
+// const jwt = require('jsonwebtoken')
 // const cookieParser = require('cookie-parser')
 const app = express()
 const port = process.env.PORT || 3000
